5ca35d7faac6a64c5c3400fd01b6d8886f2ff59e
Rafal421 <57561271+Rafal421@users.noreply.github.com>
2021-03-05 18:30:54 +0100

    EHH

--- app.js
+++ app.js
@@ -8,10 +8,4 @@ const openMenu = document.querySelector('.openMenu');
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
+
